fe6c76a27a16edb50491b421332f0af293889a2f
SaravanakumarB_Contus <saravanakumar.b@contus.in>
2023-03-25 11:39:25 +0530

    separate file for event handlers and gradle transitive removed from plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fly_chat/fly_chat.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlyChat.init(ChatBuilder(
+      domainBaseUrl: 'https://api-uikit-qa.contus.us/api/v1/',
+      licenseKey: 'ckIjaccWBoMNvxdbql8LJ2dmKqT5bp',
+      groupConfig: GroupConfig(
+          enableGroup: true,
+          maxMembersCount: 250), iOSContainerID: 'group.com.mirrorfly.qa'));
   runApp(const MyApp());
 }
 
@@ -11,9 +18,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      title: "MirrorFly",
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+        ],
+      ),
     );
   }
 }
