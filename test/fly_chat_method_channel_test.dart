fd77023ae380f8f5b875e58a7839a0f65b742ece
Mani Vendhan M <manivendhan.m@contus.in>
2023-07-01 13:53:48 +0530

    #test file change

--- test/fly_chat_method_channel_test.dart
+++ test/fly_chat_method_channel_test.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mirrorfly_plugin/builder.dart';
 import 'package:mirrorfly_plugin/fly_chat_method_channel.dart';
 
 void main() {
   MethodChannelFlyChatFlutter platform = MethodChannelFlyChatFlutter();
-  const MethodChannel channel = MethodChannel('contus.mirrorfly/flyChat');
+  // const MethodChannel channel = MethodChannel('contus.mirrorfly/flyChat');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    // channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    //   return '42';
+    // });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    // channel.setMockMethodCallHandler(null);
   });
 
   test('init', () async {
