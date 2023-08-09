862154775514ac5ca85d87d9421a07c7baff0a27
Saravanakumar <saravanakumar.b@contus.in>
2023-08-09 12:36:50 +0530

    default set audio route to speaker if make video call

--- android/src/main/kotlin/com/mirrorfly/mirrorfly_plugin/call/SdkCallFunctions.kt
+++ android/src/main/kotlin/com/mirrorfly/mirrorfly_plugin/call/SdkCallFunctions.kt
@@ -106,6 +106,7 @@ class SdkCallFunctions(var context: Context): MissedCallListener, MediaNotificat
         println("permission ${CallManager.isAudioCallPermissionsGranted(skipBlueToothPermission = false)}")
         LogMessage.d("makeVoiceCall", "permission granted ${CallManager.isAudioCallPermissionsGranted(skipBlueToothPermission = false)}")
         if (CallManager.isAudioCallPermissionsGranted(false)) {
+            CallManager.setAudioDevice(AudioDevice.SPEAKER_PHONE);
             CallManager.makeVoiceCall(userJid, object : CallActionListener {
                 override fun onResponse(isSuccess: Boolean, message: String) {
                     LogMessage.d("makeCall", "success $isSuccess message $message")
