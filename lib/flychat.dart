61c195482d1b79ef0c67480cb3d6f7aadd6239c1
Saravanakumar <saravanakumar.b@contus.in>
2023-08-25 20:40:41 +0530

    * for iOS Need to add Notification Extension Service * add this line in your extension service MirrorFlyNotification().handleNotification(notificationRequest: request, contentHandler: contentHandler, containerID: "xxx", licenseKey: "xxxx") * authToken Deprecated instead of use refreshAndGetAuthToken

--- lib/flychat.dart
+++ lib/flychat.dart
@@ -263,8 +263,10 @@ class Mirrorfly {
     return FlyChatFlutterPlatform.instance.isMuted(jid);
   }
 
-  ///This [handleReceivedMessage] used to get ChatMessage from FCM Notification
+  ///This [handleReceivedMessage] used to get ChatMessage from FCM Notification [Android] Only
   ///to Show Notification using FCM [remoteMessage.data] as [notificationData]
+  ///for [iOS] Need to add Notification Extension Service
+  ///add this line in your extension service MirrorFlyNotification().handleNotification(notificationRequest: request, contentHandler: contentHandler, containerID: "xxx", licenseKey: "xxxx")
   static Future<dynamic> handleReceivedMessage(Map notificationData) {
     return FlyChatFlutterPlatform.instance
         .handleReceivedMessage(notificationData);
