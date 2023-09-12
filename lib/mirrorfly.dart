14c5c58c645bea08e1e90f0d2da3afc024b48d55
Saravanakumar <saravanakumar.b@contus.in>
2023-09-12 19:19:31 +0530

    available_features.dart file export added

--- lib/mirrorfly.dart
+++ lib/mirrorfly.dart
@@ -19,6 +19,7 @@ export 'model/message_delivered_model.dart';
 export 'model/message_object.dart';
 export 'model/export_model.dart';
 export 'model/call_constants.dart';
+export 'model/available_features.dart';
 export 'logmessage.dart';
 export 'builder.dart';
 export 'flychat.dart';
