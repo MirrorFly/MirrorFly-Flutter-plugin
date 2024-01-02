dd7c46756e9f12cd0c9f600dcff3f3686ba47688
Saravanakumar <saravanakumar.b@contus.in>
2024-01-02 15:03:30 +0530

    kDebugMode added for log print

--- lib/logmessage.dart
+++ lib/logmessage.dart
@@ -4,7 +4,7 @@ import 'fly_chat_method_channel.dart';
 
 class LogMessage {
   static void i(String tag, dynamic message) {
-    if (MethodChannelFlyChatFlutter.enableDebugLog) {
+    if (MethodChannelFlyChatFlutter.enableDebugLog && kDebugMode) {
       // print("MirrorFly : $tag ==> $msg");
       final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
       pattern.allMatches(message.toString()).forEach(
@@ -13,7 +13,7 @@ class LogMessage {
   }
 
   static void d(String tag, dynamic message) {
-    if (MethodChannelFlyChatFlutter.enableDebugLog) {
+    if (MethodChannelFlyChatFlutter.enableDebugLog && kDebugMode) {
       // print("MirrorFly : $tag ==> $msg");
       final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
       pattern.allMatches(message.toString()).forEach(
@@ -22,7 +22,7 @@ class LogMessage {
   }
 
   static void e(String tag, dynamic message) {
-    if (MethodChannelFlyChatFlutter.enableDebugLog) {
+    if (MethodChannelFlyChatFlutter.enableDebugLog && kDebugMode) {
       // print("MirrorFly : $tag ==> $msg");
       final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
       pattern.allMatches(message.toString()).forEach(
