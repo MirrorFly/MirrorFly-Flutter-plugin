63aa0b70316cdcfd977c3320b296bcf1a273376b
Saravanakumar <saravanakumar.b@contus.in>
2024-01-18 17:19:25 +0530

    test file changes

--- test/fly_chat_method_channel_test.dart
+++ test/fly_chat_method_channel_test.dart
@@ -1,17 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mirrorfly_plugin/builder.dart';
 import 'package:mirrorfly_plugin/fly_chat_method_channel.dart';
-import 'package:mirrorfly_plugin/internal_models/available_features_model.dart';
-import 'package:mirrorfly_plugin/internal_models/call_logs_model.dart';
-import 'package:mirrorfly_plugin/internal_models/chat_messages_model.dart';
-import 'package:mirrorfly_plugin/internal_models/message_delivered_status_model.dart';
-import 'package:mirrorfly_plugin/internal_models/profile_detail_model.dart';
-import 'package:mirrorfly_plugin/internal_models/user_profile_update.dart';
-import 'package:mirrorfly_plugin/internal_models/recent_chat_model.dart';
-import 'package:mirrorfly_plugin/internal_models/register_user_model.dart';
-import 'package:mirrorfly_plugin/internal_models/get_user_profile_model.dart';
 
 void main() {
   MethodChannelFlyChatFlutter platform = MethodChannelFlyChatFlutter();
