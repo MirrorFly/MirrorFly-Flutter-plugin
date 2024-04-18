1a2119d80b2fe201a220c1850f1e4c8a21da57b7
Mani Vendhan M <manivendhan.m@contus.in>
2024-04-18 11:17:22 +0530

    #Meta Data Name Change

--- lib/model/topic_metadata.dart
+++ lib/model/topic_metadata.dart
@@ -9,24 +9,23 @@ extension ExtractTopicData on TopicMetaData {
   Map<String, dynamic> toMap() => {'key': key, 'value': value};
 }
 
-class MetaData {
-  MetaData({this.key, this.value});
-
-  String? key;
-  String? value;
+class IdentifierMetaData {
+  IdentifierMetaData({required this.key, required this.value});
+  String key;
+  String value;
 }
 
-extension ExtractMetaData on MetaData {
+extension ExtractMetaData on IdentifierMetaData {
   Map<String, dynamic> toMap() => {'key': key, 'value': value};
 }
 
 class MetaDataUserList {
-  MetaDataUserList({this.key, this.value});
+  MetaDataUserList({required this.key,required this.value});
 
-  String? key;
-  List<String>? value;
+  String key;
+  List<String> value;
 }
 
-extension ExtractMetaDataUserList on MetaData {
+extension ExtractMetaDataUserList on IdentifierMetaData {
   Map<String, dynamic> toMap() => {'key': key, 'value': value};
 }
